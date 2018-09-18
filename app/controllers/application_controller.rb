9cd94ad345dd4c588b8ba484ac2289ec1defb32f
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-18 21:53:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :cart
-  
-  def cart 
-    #this method should return an array of the items stored in the cart (utilizing the Rails session method).
 
-    session[:cart] ||= []
+  def cart
+  	session[:cart] ||= []
   end
 end
